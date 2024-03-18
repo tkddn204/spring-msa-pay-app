52ce667d1fa22fd63277139b79d486249e35b710
rightpair <tkddn204@gmail.com>
2024-03-18 13:18:08 +0900

    add common dir

---
+++ membership-service/src/main/java/net/rightpair/common/SelfValidating.java
@@ -0,0 +1,19 @@
+package net.rightpair.common;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+
+import java.util.Set;
+
+public abstract class SelfValidating<T> {
+    private final Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+
+    protected void validateSelf() {
+        Set<ConstraintViolation<T>> violations = validator.validate((T) this);
+        if (!violations.isEmpty()) {
+            throw new ConstraintViolationException(violations);
+        }
+    }
+}
